2d46bd7f05e9c6467857c167544e4b3423d5c232
banunkers <hugwan-6@student.ltu.se>
2019-09-03 10:18:21 +0200

    removed old files

--- rustTest/hello_cargo/target/debug/deps/hello_cargo-fad46eed434876f7.d
+++
@@ -1,5 +0,0 @@
-/home/banunkers/github/D7050E/rustTest/hello_cargo/target/debug/deps/hello_cargo-fad46eed434876f7: src/main.rs
-
-/home/banunkers/github/D7050E/rustTest/hello_cargo/target/debug/deps/hello_cargo-fad46eed434876f7.d: src/main.rs
-
-src/main.rs:
--- rustTest/hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/home/banunkers/github/D7050E/rustTest/hello_cargo/target/debug/hello_cargo: /home/banunkers/github/D7050E/rustTest/hello_cargo/src/main.rs
