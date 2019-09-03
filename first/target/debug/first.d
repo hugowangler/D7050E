e34eb9e3040bcd50c51ab4966e6f3e10a82eabc2
banunkers <hugwan-6@student.ltu.se>
2019-09-03 10:16:24 +0200

    testing nom

---
+++ first/target/debug/first.d
@@ -0,0 +1,1 @@
+/home/banunkers/github/D7050E/first/target/debug/first: /home/banunkers/github/D7050E/first/src/main.rs
---
+++ first/target/doc/source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["first"] = {"name":"","files":["main.rs"]};
+createSourceSidebar();
