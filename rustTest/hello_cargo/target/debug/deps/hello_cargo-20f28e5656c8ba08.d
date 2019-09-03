5187efba941e4f5bc456e048ada60fb231813432
banunkers <hugwan-6@student.ltu.se>
2019-09-03 09:41:35 +0200

    Tested some rust commands

---
+++ rustTest/hello_cargo/target/debug/deps/hello_cargo-20f28e5656c8ba08.d
@@ -0,0 +1,5 @@
+/home/banunkers/github/D7050E/rustTest/hello_cargo/target/debug/deps/hello_cargo-20f28e5656c8ba08.rmeta: src/main.rs
+
+/home/banunkers/github/D7050E/rustTest/hello_cargo/target/debug/deps/hello_cargo-20f28e5656c8ba08.d: src/main.rs
+
+src/main.rs:
---
+++ rustTest/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/banunkers/github/D7050E/rustTest/hello_cargo/target/debug/hello_cargo: /home/banunkers/github/D7050E/rustTest/hello_cargo/src/main.rs
