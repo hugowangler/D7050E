d585e198f1ff389aedd75e065142d0c4ec61794a
banunkers <hugwan-6@student.ltu.se>
2019-09-11 11:44:18 +0200

    Restructured unit tests to include types

--- parser/src/parse/let_parser.rs
+++ parser/src/parse/let_parser.rs
@@ -15,19 +15,19 @@ mod tests {
 
     #[test]
     fn test_let_number() {
-        assert!(parse("let a = 2;").is_ok());
+        assert!(parse("let a:u32 = 2;").is_ok());
     }
 
     #[test]
     fn test_let_expr() {
-        assert!(parse("let b = 2 + a;").is_ok());
+        assert!(parse("let b : i32 = 2 + a;").is_ok());
     }
 
     #[test]
     fn test_let_op() {
-        assert!(parse("let b = 1 + 2;").is_ok());   // Add
-        assert!(parse("let a = 2 - b;").is_ok());   // Sub
-        assert!(parse("let b = 2 / a;").is_ok());   // Div
-        assert!(parse("let b = 2 * a;").is_ok());   // Mul
+        assert!(parse("let b : i32 = 1 + 2;").is_ok());   // Add
+        assert!(parse("let a : u32 = 2 - b;").is_ok());   // Sub
+        assert!(parse("let b : i8 = 2 / a;").is_ok());   // Div
+        assert!(parse("let b : i16 = 2 * a;").is_ok());   // Mul
     }
 }
