731de7067b8d7e0a0289b10c425ce41ec1580b47
banunkers <hugwan-6@student.ltu.se>
2019-09-19 12:08:24 +0200

    If statement implementation. Handles if, else, else-if. TODO: Ignore whitespaces inbetween else and {

--- parser/src/main.rs
+++ parser/src/main.rs
@@ -16,7 +16,26 @@ use parse::{
 fn main() {
     //println!("{:#?}", &content_parser::parse("let a : bool = (c == b)"));
     //println!("{:#?}", &relexpr_parser::parse("a || b"));
-    println!("{:#?}", &logexpr_parser::parse("a || c > b"));
-    //println!("{:#?}", &statement_parser::parse("let node: bool = nodeA || nodeB > c; let c: u32 = a > b;"));
+    //println!("{:#?}", &logexpr_parser::parse("a || c > b"));
     //println!("{:#?}", &keyword_parser::parse("let b : bool = a + 5 > b && c"));
+    println!("{:#?}", &statement_parser::
+        parse(
+            "if (b && c) {
+                    let xyz: bool = a < b || a + 6;
+                } else if (b && d) {
+                    if (c == 5) {
+                        let a: i32 = 123123 == b;
+                    } else {
+                        let a: i32 = b && c;
+                    }
+                    let b: i32 = 15;
+                } else if (b) {
+                    let c: i32 = 12;
+                } else if (c) {
+                    let d: i32 = a && b;
+                } else {
+                    let asd: bool = true;
+                }"
+        )
+    );
 }
--- parser/src/parse/statement_parser.rs
+++ parser/src/parse/statement_parser.rs
@@ -14,43 +14,134 @@ mod tests {
     use super::parse;
 
     #[test]
-    fn test_state_1() {
-        assert!(parse("let a: i32 = 12;").is_ok())
-    }
-
-    #[test]
-    fn test_state_mult() {
-        assert!(parse("let a: i32 = 12; let a: i32 = 12; let a: i32 = 12; let a: i32 = 12;").is_ok())
-    }
-
-       #[test]
-    fn test_state_rel() {
+    fn test_state_let() {
+        assert!(parse("let a: i32 = 12;").is_ok());
+        assert!(parse("let a: i32 = 12; let a: i32 = 12; let a: i32 = 12; let a: i32 = 12;").is_ok());
         assert!(parse("let b: bool = a > 1 && b < 3;").is_ok());
-    }
-
-    #[test]
-    fn test_state_log() {
         assert!(parse("let b: bool = a && b || c && a;").is_ok());
-    }
-
-    #[test]
-    fn test_state_rellog() {
         assert!(parse("let xyz: bool = a || b && a < c;").is_ok());
         assert!(parse("let xyz: bool = a < b && a == c;").is_ok());
-
-    }
-    
-    fn test_state_exprlog() {
         assert!(parse("let xyz: bool = a && a < c;").is_ok());
         assert!(parse("let xyz: bool = a < b || a + 6;").is_ok());
     }
 
     #[test]
-    fn test_expr_number_paran() {
-        assert!(parse("let xyz: bool = a || (1);").is_ok());
-        assert!(parse("let xyz: bool = b && (1+2));").is_err());
-        assert!(parse("let xyz: bool = c && ((1+2));").is_ok());
+    fn test_state_if() {
+        assert!(parse("if (b) {let xyz: bool = a < b || a + 6;}").is_ok());
+        assert!(parse("if (b > 5) {let xyz: i32 = a < b || a + 6;}").is_ok());
+        assert!(parse("if (b && c) {let xyz: bool = a < b || a + 6;}").is_ok());
+
     }
+
+    #[test]
+    fn test_state_rec_if() {
+        assert!(
+            parse(
+                "if (b && c) {
+                    let xyz: bool = a < b || a + 6;
+
+                    if (xyz == true) {
+                        let xyz: bool = c;
+                    }
+                }"
+            ).is_ok());
+    }
+
+    #[test]
+    fn test_state_if_else() {
+        assert!(
+            parse(
+                "if (b && c) {
+                    let xyz: bool = a < b || a + 6;
+                } else {
+                    let b: i32 = 15;
+                }"
+            ).is_ok());
+    }
+
+    #[test]
+    fn test_state_rec_if_else() {
+        assert!(
+            parse(
+                "if (b && c) {
+                    let xyz: bool = a < b || a + 6;
+                } else {
+                    if (c == 5) {
+                        let a: i32 = 123123 == b;
+                    } else {
+                        let a: i32 = b && c;
+                    }
+
+                    let b: i32 = 15;
+                }"
+            ).is_ok()
+        );
+    }
+
+    #[test]
+    fn test_state_else_if() {
+        assert!(
+            parse(
+                "if (b && c) {
+                    let xyz: bool = a < b || a + 6;
+                } else if (b && d) {
+                    if (c == 5) {
+                        let a: i32 = 123123 == b;
+                    } else {
+                        let a: i32 = b && c;
+                    }
+
+                let b: i32 = 15;
+                }"
+            ).is_ok()
+        );
+    }
+
+    #[test]
+    fn test_state_else_if_else() {
+        assert!(
+            parse(
+                "if (b && c) {
+                    let xyz: bool = a < b || a + 6;
+                } else if (b && d) {
+                    if (c == 5) {
+                        let a: i32 = 123123 == b;
+                    } else {
+                        let a: i32 = b && c;
+                    }
+
+                    let b: i32 = 15;
+                } else {
+                    let asd: bool = true;
+                }"
+            ).is_ok()
+        );
+    }
+
+    #[test]
+    fn test_state_mul_else_if() {
+        assert!(
+            parse(
+                "if (b && c) {
+                    let xyz: bool = a < b || a + 6;
+                } else if (b && d) {
+                    if (c == 5) {
+                        let a: i32 = 123123 == b;
+                    } else {
+                        let a: i32 = b && c;
+                    }
+                    let b: i32 = 15;
+                } else if (b) {
+                    let c: i32 = 12;
+                } else if (c) {
+                    let d: i32 = a && b;
+                } else {
+                    let asd: bool = true;
+                }"
+            ).is_ok()
+        );
+    }
+
 }
 
 
