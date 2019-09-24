99290e509d35c1d8d08b9f5f99af8156cc6c6977
banunkers <hugwan-6@student.ltu.se>
2019-09-24 16:01:17 +0200

    changed my mind

--- src/func.rs
+++
@@ -1,9 +0,0 @@
-use crate::ast::Node;
-
-#[derive(Debug, PartialEq)]
-pub struct Function {
-    Name(String),
-    Params(Vec<Box<Node>>),
-    Args(Vec<Box<Node>>),
-    ReturnType(LiteralType)
-}
