ab37af161f3d6dfc5c0044c81929b6632b12ebf1
banunkers <hugwan-6@student.ltu.se>
2019-09-27 14:18:57 +0200

    Added progress on nodes in ast.rs

--- src/ast.rs
+++ src/ast.rs
@@ -11,24 +11,24 @@ use crate::operators::{
 
 #[derive(Clone, Debug, PartialEq)]
 pub enum Node {
-    Number(i32),
-    Bool(bool),
-    _String(String),
+	Number(i32),	// 
+	Bool(bool),	//
+    _String(String),	//
 
-    Var(String),
-    VarBinding(Box<Node>, LiteralType),
-    VarValue(Box<Node>, Box<Node>),
+    Var(String),	//
+    VarBinding(Box<Node>, LiteralType),	//
+    VarValue(Box<Node>, Box<Node>),	//
 
-    BinOp(Box<Node>, BinOpcode, Box<Node>),
-    LogOp(Box<Node>, LogOpcode, Box<Node>),
-    RelOp(Box<Node>, RelOpcode, Box<Node>),
+    BinOp(Box<Node>, BinOpcode, Box<Node>),	//
+    LogOp(Box<Node>, LogOpcode, Box<Node>),	//
+	RelOp(Box<Node>, RelOpcode, Box<Node>),	//
 
     // Keywords
-    Let(Box<Node>, Box<Node>),
-    Print(Box<Node>),
-    If{cond: Box<Node>, statement: Box<Node>},
-    IfElse{cond: Box<Node>, if_statement: Box<Node>, else_statement: Box<Node>},
-    While{cond: Box<Node>, statement: Box<Node>},
+    Let(Box<Node>, Box<Node>),	//
+    Print(Box<Node>),	//
+    If{cond: Box<Node>, statement: Box<Node>},	//
+    IfElse{cond: Box<Node>, if_statement: Box<Node>, else_statement: Box<Node>},	//
+    While{cond: Box<Node>, statement: Box<Node>},	//
 
     Func{name: Function, params: Function, r_type: Function, body: Box<Node>},
     FuncParam(Box<Node>, LiteralType),
@@ -40,5 +40,5 @@ pub enum Node {
 
     Return(Box<Node>),
 
-    Statement(Box<Node>, Box<Node>)
+    Statement(Box<Node>, Box<Node>)	//
 }
